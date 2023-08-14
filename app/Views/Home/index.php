cca875523110e431273c25e549e011ac57787c35
fares takorabt <farestakorabt@gmail.com>
2023-08-15 00:16:52 +0200

    ci4 udemy

---
+++ app/Views/Home/index.php
@@ -0,0 +1,14 @@
+
+<?= $this->extend('layouts/default') ?>
+
+<?= $this->section('title') ?>Home<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+
+<h1>Welcome to Udemy</h1>
+
+<?php
+// phpinfo();
+?>
+
+<?= $this->endSection() ?>
